d07272df266e9992df423665bc05cdc9eececbac
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-04-24 17:33:10 -0300

    feat: first
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# xdg-open-svc
+
+A simple service that you can run in your host, forward the socket over SSH, and
+use it to open URLs and etc from your target host.
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/charmbracelet/log"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":2226")
+	if err != nil {
+		log.Fatal("could not open socket", "error", err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Warn("could not accept connection", "error", err)
+			_ = conn.Close()
+			continue
+		}
+		log.Info("got conn")
+		bts, _, err := bufio.NewReader(conn).ReadLine()
+		if err != nil {
+			log.Warn("could not process connection", "error", err)
+			_ = conn.Close()
+			continue
+		}
+
+		args := strings.Fields(string(bts))
+		log.Info("got args", "args", args)
+		if err := open(args...); err != nil {
+			log.Warn("could not process request", "error", err)
+			_ = conn.Close()
+			continue
+		}
+		_ = conn.Close()
+	}
+}
+
+func open(args ...string) error {
+	switch runtime.GOOS {
+	case "darwin":
+		_, err := exec.Command("open", args...).CombinedOutput()
+		return err
+	case "linux":
+		if d := os.Getenv("DISPLAY"); d == "" {
+			return fmt.Errorf("DISPLAY not set")
+		}
+		_, err := exec.Command("xdg-open", args...).CombinedOutput()
+		return err
+	default:
+		return fmt.Errorf("unsupported OS")
+	}
+}
